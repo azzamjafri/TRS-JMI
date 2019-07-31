c0a62a1dc52af895aa31b455a2a41fd518939d07
azzamjafri <azzamjafri98@gmail.com>
2019-07-31 11:09:16 +0530

    Initial commit

---
+++ src/main/java/trs/jmi/trsjmi/TrsjmiApplication.java
@@ -0,0 +1,13 @@
+package trs.jmi.trsjmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrsjmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrsjmiApplication.class, args);
+    }
+
+}
